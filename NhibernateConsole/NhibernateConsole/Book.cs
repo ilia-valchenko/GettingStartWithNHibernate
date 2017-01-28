b895d586979c8b9cfbfd6717df95dc4e6f5772db
Ilya Valchanka <ilia.valchenko.epam@gmail.com>
2017-01-29 00:42:08 +0300

    First meeting with NHibernate.

---
+++ NhibernateConsole/NhibernateConsole/Book.cs
@@ -0,0 +1,13 @@
+﻿namespace NhibernateConsole
+{
+    public class Book
+    {
+        public virtual int Id { get; set; }
+        public virtual string Title { get; set; }
+
+        public override string ToString()
+        {
+            return $"Id: {Id}, Title: {Title}";
+        }
+    }
+}
---
+++ NhibernateConsole/NhibernateConsole/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using NHibernate;
+using NHibernate.Cfg;
+using NHibernate.Dialect;
+using System.Reflection;
+using NHibernate.Driver;
+
+namespace NhibernateConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Configuration configuration = new Configuration();
+            
+            // Another way to configuration
+            // "hibernate.cfg.xml"          
+
+            configuration.DataBaseIntegration(x => 
+            {
+                x.ConnectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Bookstore;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent =ReadWrite;MultiSubnetFailover=False";
+                x.Driver<SqlClientDriver>();
+                x.Dialect<MsSql2012Dialect>();
+            });
+
+            configuration.AddAssembly(Assembly.GetExecutingAssembly());
+            ISessionFactory factory = configuration.BuildSessionFactory();
+            ISession session = factory.OpenSession();
+
+            using (session.BeginTransaction())
+            {
+                //Book book = new Book { Id = 2, Title = "Riddick" };
+                //session.Save(book);
+                //session.Transaction.Commit();
+
+                var books = session.CreateCriteria<Book>().List<Book>();
+
+                foreach (var book in books)
+                    Console.WriteLine(book);
+            }
+
+            Console.WriteLine("\nTap to continue...");
+            Console.ReadKey(true);
+        }
+    }
+}
